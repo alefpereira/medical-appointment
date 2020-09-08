55d54956d3ba20ad36fa7370f6ed05766acddc3e
Alef Pereira <ealefpereira@gmail.com>
2020-09-07 22:46:40 -0400

    Mock api/doctor GET method in spring boot

--- src/main/java/com/alefpereira/medical_appointment/medicalappointment/DoctorController.java
+++ src/main/java/com/alefpereira/medical_appointment/medicalappointment/DoctorController.java
@@ -7,12 +7,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DoctorController {
     private static final String template = "Hello, %s!";
-    
     @RequestMapping("/api/greetings")
     public Doctor grettings (
         @RequestParam(value="name", defaultValue = "World") String name
     ) {
         return new Doctor(String.format(template, name));
+    } 
+
+    @RequestMapping("/api/doctor")
+    public String allDoctors () {
+        String res = "["
+            +"{"
+            +   " \"name\": \"Dr. August Margin\""
+            + "},{"
+            + "    \"name\": \"Dr. Joaquis Lupus\""
+            + "},{"
+            + "    \"name\": \"Dr. Xaviers Charles\""
+            + "}"
+        +"]";
+        return res;
     } 
 
 }
